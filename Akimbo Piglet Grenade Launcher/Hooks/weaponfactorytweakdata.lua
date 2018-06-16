8ba16684d1f7e2cd675c5e9226b07dcfa96153b5
Dr_Newbie <tf2baidonation@gmail.com>
2018-06-16 19:29:14 +0800

    .

--- Akimbo Piglet Grenade Launcher/Hooks/weaponfactorytweakdata.lua
+++ Akimbo Piglet Grenade Launcher/Hooks/weaponfactorytweakdata.lua
@@ -1,3 +1,1 @@
-Hooks:PostHook(WeaponFactoryTweakData, "init", "AkimboPigletGrenadeLauncher_TweakDatainit", function(wFac)	
-	wFac.parts.wpn_fps_gre_m32_beakimbo = deep_clone(wFac.parts.wpn_fps_gre_m32)
-end)
+--??
