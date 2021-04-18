3cc7b0e767dd637327a9ed4e13060460d5672abf
Dr_Newbie <tf2baidonation@gmail.com>
2021-04-18 09:11:28 +0800

    Update playerstandard.lua

--- BeAkimbo/Hooks/playerstandard.lua
+++ BeAkimbo/Hooks/playerstandard.lua
@@ -11,22 +11,18 @@ Hooks:PostHook(PlayerStandard, "_start_action_reload", "BEAKIMBO_reload_start_ac
     end
 end)
 
-local BEAKIMBO_PlayerStandard__update_reload_timers = PlayerStandard._update_reload_timers
-function PlayerStandard:_update_reload_timers(t, dt, input)
+Hooks:PreHook(PlayerStandard, "_start_action_reload", "BEAKIMBO_update_reload_timers_Pre", function(self, t)
     local is_reload_interrupted = self._queue_reload_interupt
     local is_reload_over_shotgun = self._state_data.reload_exit_expire_t and self._state_data.reload_exit_expire_t <= t
     local is_reload_over_normal = self._state_data.reload_expire_t and self._state_data.reload_expire_t <= t or is_reload_interrupted
     local is_goldeneye_reload = self:is_BEAKIMBO_goldeneye_weapon()
     local reset_stance = (is_reload_interrupted or is_reload_over_normal or is_reload_over_shotgun)and is_goldeneye_reload
-    
-    BEAKIMBO_PlayerStandard__update_reload_timers(self, t, dt, input)
+	self.__BEAKIMBO_reset_stance = true
+end)
 
-    if reset_stance then
-        log("is_reload_interrupted"..tostring(is_reload_interrupted))
-        log("is_reload_over"..tostring(is_reload_over))
-        log("is_goldeneye_reload"..tostring(is_goldeneye_reload))
-    end
-    if reset_stance then
+Hooks:PostHook(PlayerStandard, "_start_action_reload", "BEAKIMBO_update_reload_timers_Post", function(self, ...)
+    if self.__BEAKIMBO_reset_stance then
+		self.__BEAKIMBO_reset_stance = false
         self:_stance_entered()
     end
-end
+end)
